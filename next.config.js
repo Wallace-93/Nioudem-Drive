d4fa43ac60e3a7ab7b8706f4b79bd0e30e983f31
Wallace-93 <fallies.project@gmail.com>
2026-05-21 20:22:11 +0200

    Tape un message : Fix: standalone output pour Render

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
-  output: 'standalone',
 }
 
 export default nextConfig
